bef962cda276f08eda9d661d14a9302b1403e87b
joilsonlira <joilson.lira@live.com>
2023-02-18 20:27:57 -0300

    Router implementado e configurado

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+// import App from './App'
 import './index.css'
+
+//configurando React/Router
+
+import Home from './routes/Home'
+import Infectados from './routes/Infectados'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "infectados",
+    element: <Infectados />,
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/routes/Infectados.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Infectados = () => {
+    return(
+        <div>
+            <h1>Infectados</h1>
+        </div>
+    )
+}
+
+export default Infectados
